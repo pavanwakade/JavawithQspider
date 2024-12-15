158bf2b9038281f13e9b5e1c8ac7e390cbcf68ec
PavanWakade <pavanwakade199@gmail.com>
2024-12-15 20:30:30 +0530

    15/12/24

---
+++ src/Methods/chaining/Employ.java
@@ -0,0 +1,13 @@
+package Methods.chaining;
+public class Employ {
+	  String name;
+	  String Adhar; 
+	 public Name() {
+		System.out.println("Name : "+name);
+		return this;
+	}
+	 public Adhar() {
+		System.out.println("Adhar : "+Adhar);
+		return this;
+	}
+}
---
+++ src/Methods/chaining/EmployDriver.java
@@ -0,0 +1,10 @@
+package Methods.chaining;
+
+public class EmployDriver {
+    public static void main(String[] args) {
+	Employ emp=new Employ();
+	emp.Adhar="475200655840";
+	emp.name="pavan";
+	emp.Name().Adhar();
+	}
+}
